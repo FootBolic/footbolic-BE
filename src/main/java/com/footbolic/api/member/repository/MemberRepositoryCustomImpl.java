ad85eb9f7cbe6131b22fc06fee474a9c0e64b5a9
테스트 <test@teseuteuui-MacBookPro.local>
2024-05-06 15:05:37 +0900

    회원가입 기능 개발

--- src/main/java/com/footbolic/api/member/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/com/footbolic/api/member/repository/MemberRepositoryCustomImpl.java
@@ -4,10 +4,21 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import static com.footbolic.api.member.entity.QMemberEntity.memberEntity;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
+    @Override
+    public boolean existsByIdAtPlatform(String id, String platform) {
+
+        return !queryFactory.selectFrom(memberEntity)
+                .where(
+                        memberEntity.idAtProvider.eq(id)
+                                .and(memberEntity.platform.eq(platform))
+                ).fetch().isEmpty();
+    }
 }
