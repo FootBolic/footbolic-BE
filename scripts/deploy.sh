51979011da87a63663f5022065e76203a906f2d5
테스트 <test@teseuteuui-MacBookPro.local>
2024-07-20 17:19:03 +0900

    add codedeploy config files

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/footbolic/jar
+
+echo "pm2 실행중인 프로세스 종료"
+
+pm2 kill
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+pm2 start /home/footbolic/ecosystem.config.js
