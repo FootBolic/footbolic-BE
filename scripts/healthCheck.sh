d9cf473cd5617bdbe9d620b53150fc18e1c688cb
dnjsdn96 <dnjsdn96@k2web.co.kr>
2024-07-24 19:20:02 +0900

    ci cd fix final

--- scripts/healthCheck.sh
+++ scripts/healthCheck.sh
@@ -9,11 +9,11 @@ do
   UP_COUNT=$(echo $RESPONSE | grep 'SUCCESS' | wc -l)
 
   if [ $UP_COUNT -ge 1 ]
-  then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
+  then # $up_count >= 1 ("SUCCESS" 문자열이 있는지 검증)
       echo "> Health check 성공"
       break
   else
-      echo "> Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
+      echo "> Health check의 응답을 알 수 없거나 혹은 status가 SUCCESS이 아닙니다."
       echo "> Health check: ${RESPONSE}"
   fi
 
